e956155a695cd65144e15b8cd3a6a99ab70de8b5
Lukas Hajda <hajda.lukas21@gmail.com>
2025-04-06 15:53:37 +0200

    add new route

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from: " + os.environ['CONTAINER_NAME']
+    return "Hello HOME: " + os.environ['CONTAINER_NAME']
+
+@app.route('/test')
+def test():
+    return "Hello TEST: " + os.environ['CONTAINER_NAME']
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
